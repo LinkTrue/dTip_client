a903bc43d01815c9f15afdff5c77d9b7de5dd674
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-10-08 07:24:29 +0000

    add preliminary edit page

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function EditProfile() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+            Coming Soon ...
+        </div>
+    );
+}
