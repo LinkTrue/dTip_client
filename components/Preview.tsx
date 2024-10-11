215ea0654fdc1a9a8372826e899dde9ee57ce56c
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-10-11 12:29:56 +0000

    feat(identicons): and 404.

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { QRCodeSVG } from 'qrcode.react';
-import { Button } from "@/components/ui/button"
+import Blockies from 'react-blockies'
 import { Web3Item } from "@/context/GlobalStateContext";
 import EllipsifiedAddress from "@/components/EllipsifiedAddress";
 import { copyToClipboard } from '@/lib/utils'
@@ -42,89 +42,112 @@ const Preview = ({ isPreview = false }: { isPreview: boolean }) => {
     }, [userProfile.username])
 
     return (
-
         <div className={
-            `flex flex-col gap-8 p-1 items-center border border-y-blue-100 border-x-red-100 
-            ${!isPreview && 'h-screen'}`
-        }>
-
-            {/* USERNAME */}
-            <div className="bg-gray-200 rounded-full mt-10 shadow-md">
-                <p className="p-4"><strong>@ {userProfile.username}</strong></p>
-            </div>
-
-            {/* WEB2 ITEMS */}
-            <div className="flex flex-wrap justify-center mt-2 mb-8 bg-gray-300 rounded-lg p-3 gap-x-3">
-                {userProfile.web2Items.map((item, index) => (
-                    <a key={`web2Addresses${index}`} href={item.fullURL} target="_blank" rel="noopener noreferrer"
-                        title={item.iconUrl.substring(item.iconUrl.lastIndexOf('/') + 1).replace('.svg', '')}
-                    >
-                        <div className="" style={{ display: "flex", alignItems: "center", margin: "2px" }}>
-                            <Image src={item.iconUrl} alt={item.iconUrl}
-                                width={30}
-                                height={30}
-                                loading="lazy"
-                            />
+            `flex flex-col gap-8 p-1 items-center border border-y-blue-100 border-x-red-100 rounded-xl`}>
+            {
+                userProfile.username ?
+                    <>
+                        {/* Image */}
+                        <div className="bg-gray-200 rounded-full mt-5 shadow-md" title="">
+                            <Blockies className="rounded-full" seed={userProfile.username} size={12} scale={10} />
                         </div>
-                    </a>
-                ))}
-            </div>
 
-            {/* WEB3 ITEMS */}
-            <div className="flex flex-col items-center justify-center">
-                {userProfile.web3Items.length > 0 && (
-                    <div className="flex items-center space-x-4">
-                        {/* @ts-expect-error  error*/}
-                        <Web3ItemsComboBox onSelect={handleWeb3ItemSelect} />
-                    </div>
-                )}
+                        {/* USERNAME */}
+                        <div className="rounded-full shadow-sm">
+                            <p className="p-4"><strong>@ {userProfile.username || "Not Found"}</strong></p>
+                        </div>
 
-                {selectedWeb3Item.walletAddress && (
-                    <div className="flex flex-col mt-4 gap-4 items-center">
-                        <EllipsifiedAddress walletAddress={selectedWeb3Item.walletAddress} />
-                        {/* <p className="pt-4">or Scan the QR Code</p> */}
-                        <QRCodeSVG
-                            className="rounded-xl border-8 p-2 border-gray-100"
-                            size={200}
-                            level={'H'}
-                            title={`Scan the QR code to copy the selected wallet address`}
-                            imageSettings={{
-                                src: selectedWeb3Item.icon,
-                                height: 50,
-                                width: 50,
-                                excavate: false,
-                                opacity: 1
-                            }}
-                            value={selectedWeb3Item.walletAddress}
-                        />
-                    </div>
-                )}
-            </div>
+                        {/* WEB2 ITEMS */}
+                        <div className="flex flex-wrap justify-center mb-2 bg-gray-100 rounded-lg p-3 px-16 gap-x-3">
+                            {userProfile.web2Items.map((item, index) => (
+                                <a key={`web2Addresses${index}`} href={item.fullURL} target="_blank" rel="noopener noreferrer"
+                                    title={item.iconUrl.substring(item.iconUrl.lastIndexOf('/') + 1).replace('.svg', '')}
+                                >
+                                    <div className="" style={{ display: "flex", alignItems: "center", margin: "2px" }}>
+                                        <Image src={item.iconUrl} alt={item.iconUrl}
+                                            width={30}
+                                            height={30}
+                                            loading="lazy"
+                                        />
+                                    </div>
+                                </a>
+                            ))}
+                        </div>
 
+                        {/* WEB3 ITEMS */}
+                        <div className="flex flex-col items-center justify-center">
+                            {userProfile.web3Items.length > 0 && (
+                                <div className="flex items-center space-x-4">
+                                    {/* @ts-expect-error  error*/}
+                                    <Web3ItemsComboBox onSelect={handleWeb3ItemSelect} />
+                                </div>
+                            )}
 
-            {!isPreview &&
-                <div className="flex text-xl items-center justify-center gap-8 space-x-2 pt-6 ">
-                    <span className="fa-solid fa-share text-lg text-orange-500"
-                    title="Share this profile"
-                    ></span>
-                    <span className="flex space-x-4 items-center">
-                        <span title="share this profile on twitter" onClick={shareOnTwitter} className="fa-brands fa-square-x-twitter cursor-pointer"></span>
-                        <span title="share this profile on facebook" onClick={shareOnFacebook} className="fa-brands fa-square-facebook cursor-pointer"></span>
-                        <span title="share this profile on whatsapp" onClick={shareOnWhatsApp} className="fa-brands fa-square-whatsapp cursor-pointer"></span>
-                    </span>
-                    <Button
-                        size={"sm"}
-                        onClick={() => {
-                            copyToClipboard(pageUrl)
-                            toast.info("Profile address is copied.", { description: 'Paste it wherever you like.' })
-                        }}
-                    >
-                        <span className="fas fa-copy mr-1" />Copy Link
-                    </Button>
-                </div>
+                            {selectedWeb3Item.walletAddress && (
+                                <div className="flex flex-col mt-4 gap-4 items-center">
+                                    <EllipsifiedAddress walletAddress={selectedWeb3Item.walletAddress} />
+                                    {/* <p className="pt-4">or Scan the QR Code</p> */}
+                                    <QRCodeSVG
+                                        className="rounded-xl border-8 p-2 border-gray-100"
+                                        size={200}
+                                        level={'H'}
+                                        title={`Scan the QR code to copy the selected wallet address`}
+                                        imageSettings={{
+                                            src: selectedWeb3Item.icon,
+                                            height: 50,
+                                            width: 50,
+                                            excavate: false,
+                                            opacity: 1
+                                        }}
+                                        value={selectedWeb3Item.walletAddress}
+                                    />
+                                </div>
+                            )}
+                        </div>
+
+                        {/* SHARE BUTTONS */}
+                        {!isPreview &&
+                            <div className="flex flex-col gap-4 text-center">
+                                <div className="flex text-xl items-center justify-center gap-4 pt-3 ">
+                                    <span className="fa-solid fa-share text-2lg text-orange-500"
+                                        title="Share this profile"
+                                    ></span>
+                                    <span className="flex space-x-4 items-center text-2xl">
+                                        <span title="share this profile on twitter" onClick={shareOnTwitter} className="fa-brands fa-square-x-twitter cursor-pointer"></span>
+                                        <span title="share this profile on facebook" onClick={shareOnFacebook} className="fa-brands fa-square-facebook cursor-pointer"></span>
+                                        <span title="share this profile on whatsapp" onClick={shareOnWhatsApp} className="fa-brands fa-square-whatsapp cursor-pointer"></span>
+                                    </span>
+                                </div>
+                                <div>
+                                    <a
+                                        className="cursor-pointer"
+                                        onClick={() => {
+                                            copyToClipboard(pageUrl)
+                                            toast.info("Profile address is copied.", { description: 'Paste it wherever you like.' })
+                                        }}
+                                    >
+                                        <span className="fas fa-copy m-1" />Copy Link
+                                    </a>
+                                </div>
+                            </ div>
+                        }
+                    </>
+                    :
+                    <>
+                        <div className="flex flex-col gap-8 pt-10 text-center text-xl">
+                            <p className="text-4xl">404</p>
+                            <p className="">Profile Not Found!</p>
+
+                            <div className="pt-32">
+                                <a href={'/@qwe'}>
+                                    View a sample profile{' '}
+                                    <i className="fas fa-external-link-alt"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </>
             }
         </div>
-
     );
 };
 
