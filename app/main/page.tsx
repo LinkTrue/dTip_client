ee13ce33e40b1198d0eb48f845087e7ed222f8dd
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-11-12 11:00:53 +0000

    feat(landing_page): add

---
+++ app/main/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useSteps, Steps } from "../../context/StepsContext";
+
+import { Native } from '../../hooks/Native';
+import { useBlockchain } from "@/context/BlockchainProvider";
+
+import Step1 from "../../components/steps/S1Main";
+import Step2 from "../../components/steps/S2Username";
+import Step3 from "../../components/steps/S3Web2Items";
+import Step4 from "../../components/steps/S4Web3Items";
+import Step5 from "../../components/steps/S5Preview";
+import EllipsifiedWalletAddress from '@/components/EllipsifiedAddress';
+import { ZeroAddress } from "ethers";
+
+export default function Home() {
+  const { currentStep } = useSteps();
+  const { getNativeBalance } = Native();
+  const { signer, isConnected, handleDisconnectWallet } = useBlockchain();
+  const [balance, setBalance] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (isConnected && signer) {
+      if (signer.address != ZeroAddress)
+        getNativeBalance().then(setBalance).catch(err => {
+          toast.error(err)
+        })
+      else {
+        handleDisconnectWallet();
+      }
+    }
+  },
+    [isConnected, signer]
+  );
+
+  return (
+    <main className="pt-6 xl:text-4xl md:text-2xl">
+      {currentStep === Steps.Main && <Step1 />}
+      {currentStep === Steps.Username && <Step2 />}
+      {currentStep === Steps.Web2Addresses && <Step3 />}
+      {currentStep === Steps.Web3Addresses && <Step4 />}
+      {currentStep === Steps.Preview && <Step5 />}
+
+      {isConnected && (
+        <div className="flex text-left pt-20">
+          <div>
+            <EllipsifiedWalletAddress walletAddress={signer?.address} />
+            {balance && balance > 0 ? (
+              <p className="text-xs pt-2 pb-4 text-center"><strong>Balance</strong>: {balance} $avax</p>
+            ) : (
+              <p className="text-xs" title="You need some $avax to reserve your profile">
+                Low Balance! Top up some $avax to reserve your profile.
+              </p>
+            )}
+          </div>
+        </div>
+      )}
+
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,86 @@
 "use client";
-import { useEffect, useState } from "react";
-import { toast } from "sonner";
-import { useSteps, Steps } from "../context/StepsContext";
 
-import { Native } from '../hooks/Native';
-import { useBlockchain } from "@/context/BlockchainProvider";
+import Link from "next/link";
 
-import Step1 from "../components/steps/S1Main";
-import Step2 from "../components/steps/S2Username";
-import Step3 from "../components/steps/S3Web2Items";
-import Step4 from "../components/steps/S4Web3Items";
-import Step5 from "../components/steps/S5Preview";
-import EllipsifiedWalletAddress from '@/components/EllipsifiedAddress';
-import { ZeroAddress } from "ethers";
-
-export default function Home() {
-  const { currentStep } = useSteps();
-  const { getNativeBalance } = Native();
-  const { signer, isConnected, handleDisconnectWallet } = useBlockchain();
-  const [balance, setBalance] = useState<number | null>(null);
-
-  useEffect(() => {
-    if (isConnected && signer) {
-      if (signer.address != ZeroAddress)
-        getNativeBalance().then(setBalance).catch(err => {
-          toast.error(err)
-        })
-      else {
-        handleDisconnectWallet();
-      }
-    }
-  },
-    [isConnected, signer]
-  );
-
+const LandingPage = () => {
   return (
-    <main className="pt-6 xl:text-4xl md:text-2xl">
-      {currentStep === Steps.Main && <Step1 />}
-      {currentStep === Steps.Username && <Step2 />}
-      {currentStep === Steps.Web2Addresses && <Step3 />}
-      {currentStep === Steps.Web3Addresses && <Step4 />}
-      {currentStep === Steps.Preview && <Step5 />}
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <header className="text-center mb-10">
+        <div className="flex flex-col text-lg font-bold text-red-500 mb-12">
+          <p className="mb-4">Lost donations?</p>
+          <p className="mb-4">Too much platform Fees?</p>
+          <p className="mb-4">Must trust, but can't verify?</p>
+          <p className="mb-4">Payment processing taking too long?</p>
+        </div>
 
-      {isConnected && (
-        <div className="flex text-left pt-20">
+        <div className="flex justify-center my-4">
+          <img src="images/donation_neon.png" alt="Bitcoin"
+            className="w-40 h-40 opacity-70 rounded-2xl" style={{ filter: 'brightness(80%)' }} />
+        </div>
+
+        {/* {<h1 className="text-5xl font-bold mb-4"> <span>&#8383;</span></h1>} */}
+        <p className="mt-4 text-lg text-gray-600">
+          Enable Crypto Donations on <br />
+          <span
+            className="text-red-500 font-bold animate-pulse shadow-sm rounded-lg p-1 border-red-500">
+            ANY BLOCKCHAIN 
+          </span>
+          network!
+        </p>
+
+        <div className="flex justify-center my-6 opacity-55">
+          {'{'}
+          <img src="icons/chains/ada.svg" alt="supported_chain_ada" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/algo.svg" alt="supported_chain_algo" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/avax.svg" alt="supported_chain_avax" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/bnb.svg" alt="supported_chain_bnb" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/btc.svg" alt="supported_chain_btc" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/dot.svg" alt="supported_chain_dot" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/eth.svg" alt="supported_chain_eth" className="w-6 h-6 mr-1" />
+
+
+          <span className="w-6 h-6 mr-1">
+          ...
+          </span>
+
+          <img src="icons/chains/matic.svg" alt="supported_chain_matic" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/sol.svg" alt="supported_chain_sol" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/ton.svg" alt="supported_chain_ton" className="w-6 h-6 mr-1" />
+          <img src="icons/chains/trx.svg" alt="supported_chain_trx" className="w-6 h-6 mr-1" />
+          {'}'}
+
+        </div>
+
+      </header>
+
+      <section className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-md">
+        <div className="flex flex-col space-y-8">
           <div>
-            <EllipsifiedWalletAddress walletAddress={signer?.address} />
-            {balance && balance > 0 ? (
-              <p className="text-xs pt-2 pb-4 text-center"><strong>Balance</strong>: {balance} $avax</p>
-            ) : (
-              <p className="text-xs" title="You need some $avax to reserve your profile">
-                Low Balance! Top up some $avax to reserve your profile.
-              </p>
-            )}
+            <h2 className="text-3xl font-semibold mb-4">Why <span>&#8383;</span>?</h2>
+            <p className="text-lg mb-8 break-all">Cryptocurrencies offer a decentralized, secure, and transparent way to transfer value. They empower individuals to take control of their financial transactions, free from intermediaries and their associated fees.</p>
+          </div>
+          <div>
+            <h2 className="text-3xl font-semibold mb-4">Why LinkTrue?</h2>
+            <ul className="list-disc list-inside mb-8 space-y-2">
+              <li>Share a single link</li>
+              <li>Full ownership and control</li>
+              <li>Secure and transparent as <span>&#8383;</span></li>
+              <li>Free and open-source, forever</li>
+            </ul>
           </div>
         </div>
-      )}
 
+        <div className="flex justify-between">
+          <Link href="/faq" className="inline-block px-4 py-2 text-white bg-gray-500 rounded hover:bg-blue-300 mr-2">
+            Learn More
+          </Link>
+          <Link href="/main" className="inline-block px-4 py-2 text-white bg-orange-600 rounded hover:bg-blue-700">
+            MINT yours now!
+          </Link>
+        </div>
+      </section>
     </main>
   );
-}
+};
+
+export default LandingPage; 
