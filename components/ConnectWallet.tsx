35fb6ec28282a8f4c270670ddcde5580e38fdad0
TSX <miladtsx@gmail.com>
2025-04-15 16:26:38 +0330

    refactor(design): main intro

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -2,34 +2,29 @@ import React from "react";
 import { useBlockchain } from "@/context/BlockchainProvider";
 
 const BlockchainComponent: React.FC = () => {
-    const {
-        handleConnectWallet,
-        isConnecting,
-        isConnected,
-    } = useBlockchain();
+  const { handleConnectWallet, isConnecting, isConnected } = useBlockchain();
 
-    return (
-        <div>
-            {/* Button to connect the wallet */}
-            {!isConnected && (
-                <button
-                    className="
+  return (
+    <div>
+      {/* Button to connect the wallet */}
+      {!isConnected && (
+        <button
+          className="
                     rounded-full border border-solid border-transparent
                      transition-colors flex items-center justify-center
                       bg-foreground text-background gap-2
-                       hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base 
+                       hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base
                        h-10 sm:h-12 px-4 sm:px-5
+                       animate-float
+                       hover-effect
                        "
-                    onClick={() => handleConnectWallet(true)}
-                >
-                    {isConnecting ? (
-                        (<div className="spinner"></div>)
-                    ) : 'Connect Wallet'}
-
-                </button>
-            )}
-        </div>
-    );
+          onClick={() => handleConnectWallet(true)}
+        >
+          {isConnecting ? <div className="spinner"></div> : "Connect Wallet"}
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default BlockchainComponent;
--- components/steps/S1Main.tsx
+++ components/steps/S1Main.tsx
@@ -1,139 +1,240 @@
-"use client"
-import { useSteps } from '@/context/StepsContext'
+"use client";
+import { useSteps } from "@/context/StepsContext";
 import { useBlockchain } from "@/context/BlockchainProvider";
-import BlockchainComponent from '../ConnectWallet';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { useContractMethods } from '@/hooks/useContractMethods';
-import { useGlobalState } from '@/context/GlobalStateContext';
-import { parseProfileData } from '@/lib/utils';
-import { useLogger } from '@/context/LoggerContext';
-
+import BlockchainComponent from "../ConnectWallet";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useContractMethods } from "@/hooks/useContractMethods";
+import { useGlobalState } from "@/context/GlobalStateContext";
+import { parseProfileData } from "@/lib/utils";
+import { useLogger } from "@/context/LoggerContext";
 
 const S1Main = () => {
-    const { logException } = useLogger();
-    const { nextStep } = useSteps();
-    const { signer, isConnected, isConnecting } = useBlockchain();
-    const { userProfile, setUserProfile } = useGlobalState();
+  const { logException } = useLogger();
+  const { nextStep } = useSteps();
+  const { signer, isConnected, isConnecting } = useBlockchain();
+  const { userProfile, setUserProfile } = useGlobalState();
 
-    const { getProfileByWallet, isSmartContractInitialized } = useContractMethods();
-    const [isOwner, setIsOwner] = useState<boolean>(false);
+  const { getProfileByWallet, isSmartContractInitialized } =
+    useContractMethods();
+  const [isOwner, setIsOwner] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (isSmartContractInitialized && signer?.address) {
-            getProfileByWallet(signer.address).then(profile => {
-                const cleanedData = parseProfileData(profile);
-                if (cleanedData.username) {
-                    setUserProfile(
-                        {
-                            avatar: '',
-                            username: cleanedData.username,
-                            web2Items: cleanedData.web2Items,
-                            web3Items: cleanedData.web3Items
-                        }
-                    )
-                    setIsOwner(true);
-                }
-            }).catch(err => {
-                logException(err)
-            })
-        }
-    }, [isSmartContractInitialized, signer]);
+  useEffect(() => {
+    if (isSmartContractInitialized && signer?.address) {
+      getProfileByWallet(signer.address)
+        .then((profile) => {
+          const cleanedData = parseProfileData(profile);
+          if (cleanedData.username) {
+            setUserProfile({
+              avatar: "",
+              username: cleanedData.username,
+              web2Items: cleanedData.web2Items,
+              web3Items: cleanedData.web3Items,
+            });
+            setIsOwner(true);
+          }
+        })
+        .catch((err) => {
+          logException(err);
+        });
+    }
+  }, [isSmartContractInitialized, signer]);
 
-
-    return (
-        isConnecting ?
-            <div className='text-center'>
-                <div className='spinner'></div>
+  return isConnecting ? (
+    <div className="text-center">
+      <div className="spinner"></div>
+    </div>
+  ) : (
+    <div className="flex flex-col justify-start gap-y-8 text-center rounded-lg">
+      {!isConnected && (
+        <div className="max-w-6xl mx-auto p-8 bg-white shadow-xs rounded-lg">
+          {/* Steps Grid */}
+          <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_1fr_auto_1fr] gap-10 items-center">
+            {/* Step 1 */}
+            <div className="flex flex-col items-center p-8 bg-gray-50 rounded-2xl shadow-md hover:shadow-xl transition-shadow animate-float hover-effect">
+              <div className="w-16 h-16 flex items-center justify-center bg-blue-200 rounded-full mb-6">
+                <span className="text-3xl font-bold text-blue-600">1</span>
+              </div>
+              <h2 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
+                Connect
+              </h2>
+              <p className="text-base md:text-md text-gray-600 text-center">
+                YOUR crypto wallet
+              </p>
             </div>
-            :
-            <div className='flex flex-col justify-start gap-y-14  p-4 text-center bg-gray-50 rounded-lg shadow-md'>
-                {!isConnected &&
-                    <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg">
-                        <p className="text-lg font-semibold text-gray-800 mb-2">
-                            Show which blockchains you use and accept donations
-                        </p>
-                        <p className="text-lg text-gray-600 mb-4">In 3 steps:</p>
-                        <ol className="list-decimal list-inside space-y-2 text-gray-700">
-                            <li className="flex items-center">
-                                <span className="mr-2 text-blue-500">&#9673;</span>
-                                Connect your wallet
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2 text-green-500">&#9673;</span>
-                                Mint your profile
-                            </li>
-                            <li className="flex items-center">
-                                <span className="mr-2 text-purple-500">&#9673;</span>
-                                Share your link
-                            </li>
-                        </ol>
-                    </div>
-                }
 
-                <ul className="list-inside 
+            {/* Horizontal Arrow between Step 1 and Step 2 */}
+            <div className="hidden md:block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </div>
+            <div className="block md:hidden w-full text-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-gray-500 inline-block"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+
+            {/* Step 2 */}
+            <div className="flex flex-col items-center p-8 bg-gray-50 rounded-2xl shadow-md hover:shadow-xl transition-shadow animate-float2 hover-effect">
+              <div className="w-16 h-16 flex items-center justify-center bg-green-200 rounded-full mb-6">
+                <span className="text-3xl font-bold text-green-600">2</span>
+              </div>
+              <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
+                Create
+              </h3>
+              <p className="text-base md:text-lg text-gray-600 text-center">
+                YOUR profile
+              </p>
+            </div>
+
+            {/* Horizontal Arrow between Step 2 and Step 3 */}
+            <div className="hidden md:block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-gray-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+            </div>
+            <div className="block md:hidden w-full text-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-gray-500 inline-block"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </div>
+
+            {/* Step 3 */}
+            <div className="flex flex-col items-center p-8 bg-gray-50 rounded-2xl shadow-md hover:shadow-xl transition-shadow animate-float3 hover-effect">
+              <div className="w-16 h-16 flex items-center justify-center bg-purple-200 rounded-full mb-6">
+                <span className="text-3xl font-bold text-purple-600">3</span>
+              </div>
+              <h3 className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
+                Share
+              </h3>
+              <p className="text-base md:text-lg text-gray-600 text-center">
+                YOUR dTip Link
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <ul
+        className="list-inside
             font-thin md:text-xl lg:text-2xl xl:text-2xl
-            text-center font-[family-name:var(--font-geist-mono)]">
-                    {isConnected &&
-                        <li className="mb-2">
-                            <span>
-                                {
-                                    isOwner ?
-                                        <>
-                                            Welcome {userProfile.username}!
-                                            <br />
-                                            <br />
-                                            You can now
-                                            <Link href="/profile/edit" className='text-orange-400'>
-                                                <b> Adjust </b>
-                                            </Link>
-                                            your profile.
-                                        </>
-                                        :
-                                        <>
-                                            A BioLink You Control, Forever.
-                                        </>
-                                }
-                            </span>
-                        </li>
-                    }
-                </ul>
-                {isConnected &&
-                    <span className="p-10">
-                        {
-                            isOwner ?
-                                <Link href={`/${userProfile.username}`} target='_blank'>
-                                    View your profile{' '}
-                                    <i className="fas fa-external-link-alt"></i>
-                                </Link> :
-                                <Link href={'/@miladtsx'} target='_blank'>
-                                    Example Profile{' '}
-                                    <i className="fas fa-external-link-alt"></i>
-                                </Link>
-                        }
-                    </span>
-                }
+            text-center font-[family-name:var(--font-geist-mono)]"
+      >
+        {isConnected && (
+          <li className="mb-2">
+            <span>
+              {isOwner ? (
+                <>
+                  Welcome {userProfile.username}!
+                  <br />
+                  <br />
+                  You can now
+                  <Link href="/profile/edit" className="text-orange-400">
+                    <b> Adjust </b>
+                  </Link>
+                  your profile.
+                </>
+              ) : (
+                <>Your Donation hub, Free Forever!</>
+              )}
+            </span>
+          </li>
+        )}
+      </ul>
+      {isConnected && (
+        <span className="p-10">
+          {isOwner ? (
+            <Link href={`/${userProfile.username}`} target="_blank">
+              View your profile <i className="fas fa-external-link-alt"></i>
+            </Link>
+          ) : (
+            <Link href={"/@miladtsx"} target="_blank">
+              See an Example Profile{" "}
+              <i className="fas fa-external-link-alt"></i>
+            </Link>
+          )}
+        </span>
+      )}
 
-                <div>
-                    <div className="flex gap-2 items-center flex-col">
-                        <BlockchainComponent />
-                        {isConnected && !isOwner && (
-                            <div className="flex gap-2 items-center flex-col">
-                                <p>by using <i>dTip</i>, <span className='font-bold'>you agree</span> to <a
-                                    className='underline hover:no-underline' target='_blank' href="https://dtip.gitbook.io/docs/terms/terms-of-use">TOU</a> and <a
-                                        className='underline hover:no-underline' target='_blank' href="https://dtip.gitbook.io/docs/terms/privacy">PP</a>
-                                </p>
-                                <button
-                                    className="rounded-full border-2 border-white transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                                    onClick={nextStep}
-                                >
-                                    Mint your profile
-                                </button>
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-    );
+      <div className="flex gap-2 items-center flex-col">
+        {/* BLOCKCHAIN COMPONENT */}
+        <BlockchainComponent />
+
+        {isConnected && !isOwner && (
+          <div className="flex gap-2 items-center flex-col text-sm">
+            <p>
+              <span className="font-bold">If you agree</span> to{" "}
+              <a
+                className="underline hover:no-underline"
+                target="_blank"
+                href="https://dtip.gitbook.io/docs/terms/terms-of-use"
+              >
+                Terms Of Use
+              </a>{" "}
+              and{" "}
+              <a
+                className="underline hover:no-underline"
+                target="_blank"
+                href="https://dtip.gitbook.io/docs/terms/privacy"
+              >
+                Privacy Policy
+              </a>
+            </p>
+            <button
+              className="rounded-full border-2 border-white transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              onClick={nextStep}
+            >
+              Mint your profile
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default S1Main;
